015ac7116284624f851ae0a8e35524d104ed3ea0
sann4649 <93958204+sann4649@users.noreply.github.com>
2021-11-28 19:34:24 +0900

    Update uranai.py

--- uranai.py
+++ uranai.py
@@ -271,7 +271,7 @@ def uranai(input_text):
         s = str(sum(int(x) for x in frame['birthday']))
         if int(s) % 11 == 0:
           break
-        return 'ソウルナンバーは{frame['birthday']}'    
+        return 'ソウルナンバーは'    
   return output_text
   
 
